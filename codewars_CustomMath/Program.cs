b4f4d0644e676a04b235543d25a75fabe9956764
benjaminc137 <benjamin.salasek@icloud.com>
2018-07-06 10:48:04 -0500

    initial commit

---
+++ codewars_CustomMath/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace codewars_CustomMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello math!");
+        }
+    }
+}
